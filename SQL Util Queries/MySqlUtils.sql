dafc0407b081d1f84debd4748a1ad2fe38c7e637
Carlos Dominguez <krlosnando@gmail.com>
2014-04-29 21:09:27 -0700

    Crear usuario Mysql

--- SQL Util Queries/MySqlUtils.sql
+++ SQL Util Queries/MySqlUtils.sql
@@ -1,3 +1,12 @@
 CREATE USER 'cenfocrm_user'@'localhost' IDENTIFIED BY PASSWORD '*CA2DA475C2D16F3F6BCD49B0D36373CF25C590C1';
 CREATE USER 'cenfocrm_user'@'localhost' IDENTIFIED BY '000797'; 
 GRANT ALL PRIVILEGES ON `cenfocrm\_db`.* TO 'cenfocrm_user'@'localhost';
+
+
+--Sirve
+drop user 'cenfocrm_db'@'localhost';
+flush privileges;
+CREATE USER 'cenfocrm_db'@'localhost' IDENTIFIED BY '000797'; 
+
+--Despues del comando anterior se puede agregar a la base de datos dando click en privilegio, despues poner el 
+--usuario y darle click a modificar y "All privileges".
